d1d87555816065e6e7b7facef0742c735b340150
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-06-14 18:57:49 +0300

    misc(deploy): downgrade gulp file to es5

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('build', () => {
+gulp.task('build', function () {
     gulp.src('node_modules/jquery/dist/jquery.min.js')
         .pipe(gulp.dest('scripts/lib'));
 });
