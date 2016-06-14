f2cb76912466ba595f0b1c6ae400d572c96a5cb0
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-06-14 20:03:13 +0300

    test

--- deploy.cmd
+++ deploy.cmd
@@ -23,7 +23,7 @@ setlocal enabledelayedexpansion
 SET ARTIFACTS=%~dp0%..\artifacts
 
 IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%\dist
+  SET DEPLOYMENT_SOURCE=%~dp0%
 )
 
 IF NOT DEFINED DEPLOYMENT_TARGET (
@@ -110,9 +110,9 @@ IF EXIST "%DEPLOYMENT_SOURCE%\gulpfile.js" (
 
 :: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  echo deploying from %DEPLOYMENT_SOURCE%
+  echo deploying from %DEPLOYMENT_SOURCE%\dist
 
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
